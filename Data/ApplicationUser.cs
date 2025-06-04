ddd98554ad4261d84768765ed48a8735d699b9ea
miron-kwiatkowski <miron.kwiatkowski@studenci.collegiumwitelona.pl>
2025-06-05 01:30:42 +0200

    added database

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TwoFA.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Klucz do weryfikacji dwustopniowej
+        public string TwoFactorSecret { get; set; }
+
+        // Flaga oznaczająca, czy użytkownik ma włączone 2FA
+        public bool IsTwoFactorEnabled { get; set; }
+
+        // Dodatkowe pola na potrzeby logowania zewnętrznego (np. Google)
+        public string GoogleAuthenticatorKey { get; set; }
+    }
+}
