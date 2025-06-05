6b4da60ba1d962c21651d952bf70b702c2325f64
miron-kwiatkowski <miron.kwiatkowski@studenci.collegiumwitelona.pl>
2025-06-05 16:08:56 +0200

    changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace TwoFA.Controllers
             return View();
         }
 
+        public IActionResult TwoFactorPage()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
